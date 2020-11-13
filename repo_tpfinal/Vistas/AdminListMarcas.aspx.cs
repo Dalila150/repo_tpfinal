f2ded839150380bac4e9ce852da192018faebea8
wandasscarpato <wandasscarpato@gmail.com>
2020-11-13 20:51:31 -0300

    Agrego vista listar y editar marca

---
+++ repo_tpfinal/Vistas/AdminListMarcas.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vistas
+{
+    public partial class AdminListMarcas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            //NegocioProducto gProd = new NegocioProducto();
+
+            //if (txtBuscar.Text != "")
+            //{
+            //    grdCategorias.DataSource = gProd.BuscarProductos(txtBuscar.Text);
+            //    grdCategorias.DataBind();
+            //}
+            //else
+            //{
+            //    grdCategorias.DataSource = gProd.TodosLosProductos();
+            //    grdCategorias.DataBind();
+            //}
+        }
+    }
+}
